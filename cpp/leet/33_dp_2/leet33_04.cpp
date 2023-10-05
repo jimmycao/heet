01326fa4f7f8d4262ea89871562f55fb7cc3a262
jimmycao992 <amao.chj@qq.com>
2023-10-05 08:36:28 +0800

    updates

---
+++ cpp/leet/33_dp_2/leet33_04.cpp
@@ -0,0 +1,33 @@
+#include "../utils/header_tools.h"
+#include "../utils/print_tools.h"
+
+class Solution {  // 自底向上 + DP + 记忆
+public:
+    int noDescendSeqLen(vector<int>& nums) {
+        vector<int> memo(nums.size(), 1);
+
+        for (int c = 1; c < nums.size(); c++) {
+            for (int p = 0; p < c; ++p) {
+                if (nums[p] < nums[c]) {
+                    memo[c] = std::max(memo[c], memo[p]+1);  // p被修改
+                }
+            }
+            std::cout << "c:" << c << ", memo[c]:" << memo[c] << std::endl;
+        }
+
+        return memo[nums.size()-1];
+    }
+};
+
+void test1() {
+    vector<int> nums {1, 5, 3, 4, 6, 9, 7, 8};
+
+    Solution sn;
+    int result = sn.noDescendSeqLen(nums);
+    std::cout << "result:" << result << std::endl;
+}
+
+int main() {
+    test1();
+    return 0;
+}
