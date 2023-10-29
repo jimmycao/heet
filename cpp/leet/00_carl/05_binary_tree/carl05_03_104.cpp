166e94f686726d076acda7194f56e6235e6ab88c
jimmycao992 <amao.chj@qq.com>
2023-10-29 23:16:21 +0800

    updates

--- cpp/leet/00_carl/05_binary_tree/carl05_03_104.cpp
+++ cpp/leet/00_carl/05_binary_tree/carl05_03_104.cpp
@@ -2,21 +2,27 @@
 
 #include "../../utils/tree.h"
 
-class Solution1 {
+class Solution {
 public:
-    int maxDepth(TreeNode* root) {
+    int minDepth(TreeNode *root) {
         if (!root) {
             return 0;
         }
 
         queue<TreeNode*> myQueue;
         myQueue.push(root);
-        int step = 0;
+
+        int step = 1;
         while (!myQueue.empty()) {
             size_t size = myQueue.size();
             for (int i = 0; i < size; i++) {
                 auto tmp = myQueue.front();
                 myQueue.pop();
+
+                if (!tmp->left && !tmp->right) {
+                    return step;
+                }
+
                 if (tmp->left) {
                     myQueue.push(tmp->left);
                 }
@@ -26,24 +32,33 @@ public:
             }
             step++;
         }
+
         return step;
     }
 };
 
-class Solution {
+class Solution2 {
 public:
-    int maxDepth(TreeNode* root) {
-        return getHeight(root);
+    int minDepth(TreeNode* root) {
+        return getMinDepth(root);
     }
 
-    //postOrder
-    int getHeight(TreeNode* node) {
+    int getMinDepth(TreeNode* node) {
         if (!node) {
             return 0;
         }
-        int leftH = getHeight(node->left);
-        int rightH = getHeight(node->right);
-        return 1 + std::max(leftH, rightH);
+
+        int left = getMinDepth(node->left);
+        int right = getMinDepth(node->right);
+
+        if (left == 0 && right != 0) {
+            return 1 + right;
+        }
+        if (left != 0 && right == 0) {
+            return 1 + left;
+        }
+
+        return 1 + min(left, right);
     }
 };
 
@@ -52,18 +67,21 @@ void test1() {
     TreeNode* root = new TreeNode(3, new TreeNode(9), twenty);
 
     Solution sn;
-    int result = sn.maxDepth(root);
+    int result = sn.minDepth(root);
     std::cout << "result:" << result << std::endl;
-    std::cout << ", expected:3" << std::endl;
+    std::cout << ", expected:2" << std::endl;
 }
 
 void test2() {
-    TreeNode* root = new TreeNode(1, nullptr, new TreeNode(2));
+    TreeNode* five = new TreeNode(5, nullptr, new TreeNode(6));
+    TreeNode* four = new TreeNode(4, nullptr, five);
+    TreeNode* three = new TreeNode(3, nullptr, four);
+    TreeNode* two = new TreeNode(2, nullptr, three);
 
     Solution sn;
-    int result = sn.maxDepth(root);
+    int result = sn.minDepth(two);
     std::cout << "result:" << result << std::endl;
-    std::cout << ", expected:2" << std::endl;
+    std::cout << ", expected:5" << std::endl;
 }
 
 int main() {
